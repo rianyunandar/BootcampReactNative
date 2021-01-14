e8937072c075849b3ef5a2958f36c47e1a5a5f3f
rianyunandar <rianyunandar@gmail.com>
2021-01-14 08:58:19 +0700

    tugas 4 done

---
+++ Tugas 4 – Functions/function.js
@@ -0,0 +1,41 @@
+console.log("jawaban 1")
+
+function teriak() {
+    return "Halo Sanbers!"
+}
+
+console.log(teriak())
+
+console.log("--------------------------")
+
+
+console.log("jawaban 2")
+
+function kalikan(x, y) {
+    return x * y
+}
+var num1 = 12
+var num2 = 4
+
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+
+console.log("--------------------------")
+
+console.log("jawaban 3")
+
+var name = "Agus"
+var age = 30
+var address = "Jln. Malioboro, Yogyakarta"
+var hobby = "Gaming"
+
+function introduce(name, age, address, hobby) {
+    return "Nama saya " + name + " , umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!"
+}
+
+
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan)
+
+
+console.log("--------------------------")
