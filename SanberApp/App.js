54b401e55bc5ec9c8c5f741676afcbbe30807059
rianyunandar <rianyunandar@gmail.com>
2021-01-27 08:09:58 +0700

    debug ke Upper Case

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import youtubeUI from './Tugas/Tugas12/app';
+import YoutubeUI from './tugas/tugas12/app';
+
 export default function App() {
   return (
-    <youtubeUI />
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+             <YoutubeUI />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
